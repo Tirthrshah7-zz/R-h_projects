49f1c720dddf45a3fd60a4805cd420cc510ec4b1
Tirthrshah7 <tirthrshah7@gmail.com>
2021-01-06 16:25:49 +0530

    owl carousel adds

--- src/js/owl_Carousel.js
+++ src/js/owl_Carousel.js
@@ -2,9 +2,9 @@ $('.owl-carousel').owlCarousel({
     loop: true,
     margin: 20,
     nav: true,
-    autoplay: false,
+    autoplay: true,
     dots: true,
-    // autoplayTimeout: 5000,
+    autoplayTimeout: 5000,
     autoplayHoverPause: true,
     responsive: {
         0: {
